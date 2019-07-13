8fc1af68cb3dfcb91413f69acd076e95454b7920
Damien CUVILLIER <dacuvill@e1r3p3.42.fr>
2019-07-13 16:53:21 +0200

    correction leaks double perso dernier elem de list

--- src/free_lines.c
+++ src/free_lines.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_lines.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rle-ru <rle-ru@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dacuvill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 17:50:33 by rle-ru            #+#    #+#             */
-/*   Updated: 2019/06/22 09:06:16 by rle-ru           ###   ########.fr       */
+/*   Updated: 2019/07/13 16:51:34 by dacuvill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,7 @@ void	free_lines(t_wolf *w)
 		if (cur->line)
 			ft_memdel((void**)&cur->line);
 		cur = cur->next;
-		if (cur)
-			ft_memdel((void**)&tmp);
+		ft_memdel((void**)&tmp);
 	}
 	w->parser.last_line = NULL;
 	w->parser.lines = NULL;
